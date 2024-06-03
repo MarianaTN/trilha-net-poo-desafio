502d514a197a9d832521eda4ce2be58c537f9d7c
Mariana Trevizani <mariana.trevizani@yahoo.com.br>
2024-06-02 21:37:29 -0300

    implementação da função instalarAplicativo e nova classe referente a Samsung.

---
+++ Models/Samsung.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesafioPOO.Models
+{
+    public class Samsung : Smartphone
+    {
+        public Samsung(string numero, string modelo, string imei, int memoria)
+            : base(numero, modelo, imei, memoria)
+        { }
+
+        public override void InstalarAplicativo(string app)
+        {
+            Console.WriteLine($"Instalando o aplicativo {app} no Samsung.");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,3 +1,18 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
+
+Nokia nokia = new Nokia("123456789", "Nokia 3310", "111222333444555", 64);
+nokia.Ligar();
+nokia.ReceberLigacao();
+nokia.InstalarAplicativo("WhatsApp");
+
+Iphone iphone = new Iphone("123458967", "Iphone 15", "11122239329555", 128);
+iphone.Ligar();
+iphone.ReceberLigacao();
+iphone.InstalarAplicativo("Instagram");
+
+Samsung samsung = new Samsung("023458967", "S20 FE", "1153533444550", 128);
+samsung.Ligar();
+samsung.ReceberLigacao();
+samsung.InstalarAplicativo("Telegram");
